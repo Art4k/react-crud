560ed155ac19fd85269c80d37e462b3d797944ce
Artūras <arturas008@gmail.com>
2023-02-13 14:16:48 +0200

    feat: simple api data render

--- client/src/pages/home-page.tsx
+++ client/src/pages/home-page.tsx
@@ -1,7 +1,22 @@
-import React from 'react';
+import React from "react";
+import { Box } from "@mui/material";
+import ApiService from "services/api-service";
 
-const HomePage = () => (
-  <div>HomePage</div>
-);
+const HomePage = () => {
+  const [player, setPlayer] = React.useState<PlayerModel[]>([]);
+
+  React.useEffect(() => {
+    (async () => {
+      const fetchPlayerData = await ApiService.getPlayersData();
+      setPlayer(fetchPlayerData);
+    })();
+  }, []);
+
+  return (
+    <Box>
+      <Box component="pre">{JSON.stringify(player, null, 4)}</Box>
+    </Box>
+  );
+};
 
 export default HomePage;
