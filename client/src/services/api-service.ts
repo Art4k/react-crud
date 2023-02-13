a021c20f8a5e0fa2e934441eb2526c2565a68262
Artūras <arturas008@gmail.com>
2023-02-13 13:59:12 +0200

    feat: fetch data from server

---
+++ client/src/services/api-service.ts
@@ -0,0 +1,33 @@
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: "http://localhost:5173",
+  timeout: 1000,
+  headers: {
+    Accept: "application/json",
+    "Content-Type": "application/json",
+  },
+});
+
+const getPlayersData = async () => {
+  const { data: getPlayer } = await api.get<PlayerModel[]>("/characters");
+  return getPlayer;
+};
+
+const getMonstersData = async () => {
+  const { data: getMonster } = await api.get<MonsterModel[]>("/monsters");
+  return getMonster;
+};
+
+const getItemsData = async () => {
+  const { data: getItem } = await api.get<ItemModel[]>("/monsters");
+  return getItem;
+};
+
+const ApiService = {
+  getPlayersData,
+  getMonstersData,
+  getItemsData,
+};
+
+export default ApiService;
